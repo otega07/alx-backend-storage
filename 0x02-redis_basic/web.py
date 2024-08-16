c5fe08ad064156d8fa7ea09cd7d6fcd1203b5c27
otega07 <okiriotega@gmail.com>
2024-08-16 09:59:18 +0100

    adv

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,17 +16,22 @@ def data_cacher(method: Callable) -> Callable:
     """Caches the output of fetched data.
     """
     @wraps(method)
-    def invoker(url) -> str:
+    def invoker(url: str) -> str:
         """The wrapper function for caching the output.
         """
+        # Increment the request count
         redis_store.incr(f'count:{url}')
+        
+        # Check if the result is already cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
+        
+        # Fetch the result, cache it, and return it
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
+    
     return invoker
 
 
